ab27c6a3c7d33e698f891cbbddf7d85296f9af51
wh <1455420276@qq.com>
2018-11-18 19:43:31 +0800

    用脚手架搭建vue环境

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>hello word</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-import $ from "jquery";
-import "./js/index.js";
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
